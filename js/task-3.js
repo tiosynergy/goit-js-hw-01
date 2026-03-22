c745fd10b871332994938fd3447881c976a14f78
tiosynergy <tiosynergy@gmail.com>
2026-03-22 12:38:44 +0200

    task1,2,3 fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border) {
-let totalWidth = Number(content.replace("px", "")) + Number(padding.replace("px", "")) * 2 + Number(border.replace("px", "")) * 2;
-  alert(totalWidth);
+const totalWidth = Number(content.replace("px", "")) + Number(padding.replace("px", "")) * 2 + Number(border.replace("px", "")) * 2;
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
